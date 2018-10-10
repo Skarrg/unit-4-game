dcb6cb5805bb12d9970bf7aa4a6e08cddc08ae84
Skarrggit config --global user.name Skarrggit config --global user.name Skarrg <orestes.moiran@gmail.com>
2018-10-09 19:21:18 -0700

    no functionality, just basic divs and comment pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    
+    var characters = [
+        {name: fighter1, picture:"", health: 100, attack: 10, counterAttack: 10},
+        {name: fighter2, picture:"", health: 100, attack: 10, counterAttack: 10},
+        {name: fighter3, picture:"", health: 100, attack: 10, counterAttack: 10},
+        {name: fighter4, picture:"", health: 100, attack: 10, counterAttack: 10}
+    ]
+
+    //global variables for refernerenenerece
+    var attack = 0;
+    var enemyAlive = false;
+    var playerAlive = false;
+    var gameStart = false;
+    var enemies = characters.length-1;
+
+    //start game
+
+    //set up divs for game
+
+    //character select
+
+    //enemy select
+    
+    //move enemy to combat
+
+    //combat occurs
+
+    //enemy dies
+
+    //player dies
+
+    //player defeats all characters
+
+    //player character 1
+
+    //player character 2
+
+    //player character 3
+
+    //player character 4
+})
