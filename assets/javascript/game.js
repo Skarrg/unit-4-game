5aed5ac799b378229da4da3e59e299303b6e7ba0
Justin Honegger <justinhonegger@doublefine.com>
2018-10-11 16:17:19 -0700

    sigh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,37 @@
+
+
+var characters = {
+    "wizard": { name: "Wizard", health: 100, attack: 10, counterAttack: 10 },
+    "paladin": { name: "Paladin", health: 100, attack: 10, counterAttack: 10 },
+    "bard": { name: "Bard", health: 100, attack: 10, counterAttack: 10 },
+    "warrior": { name: "Warrior", health: 100, attack: 10, counterAttack: 10 }
+}
+
+//global variables for refernerenenerece
+var attack = 0;
+var enemySet = false;
+var playerSet = false;
+var gameStart = false;
+
+
 $(document).ready(function () {
-    
-    var characters = {
-        "wizard": {name: "Wizard", health: 100, attack: 10, counterAttack: 10},
-        "paladin": {name: "Paladin", health: 100, attack: 10, counterAttack: 10},
-        "bard": {name: "Bard", health: 100, attack: 10, counterAttack: 10},
-        "warrior": {name: "Warrior", health: 100, attack: 10, counterAttack: 10}
-    }
-
-    //global variables for refernerenenerece
-    var attack = 0;
-    var enemySet = false;
-    var playerSet = false;
-    var gameStart = false;
-    
-
-
     //start game
 
     //set up divs for game
 
     //character select
-
+$('.character').click(function() {
+    if (playerSet == false) {
+        for (var i =0; i < characters.length; i++) {
+            if (characters[i] == this) {
+                $('#player').append(characters[i]);
+                playerSet = true;
+            }
+        }
+    }
+})
     //enemy select
-    
+
     //move enemy to combat
 
     //combat occurs
