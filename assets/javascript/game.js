5896f924730880fb9480e23000804a5fd65e8e54
Skarrggit config --global user.name Skarrggit config --global user.name Skarrg <orestes.moiran@gmail.com>
2018-10-11 21:01:14 -0700

    oh dang it a / in the beginning of a script source means it won't load

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,10 @@
 
 
 var characters = {
-    "wizard": { name: "Wizard", health: 100, attack: 10, counterAttack: 10 },
-    "paladin": { name: "Paladin", health: 100, attack: 10, counterAttack: 10 },
-    "bard": { name: "Bard", health: 100, attack: 10, counterAttack: 10 },
-    "warrior": { name: "Warrior", health: 100, attack: 10, counterAttack: 10 }
+    "wizard": { name: "Wizard", image: '<img src="assets/images/hero_wizard.png">', health: 80, attack: 30, counterAttack: 10 },
+    "paladin": { name: "Paladin", image: '<img src="assets/images/hero_paladin.png">', health: 200, attack: 10, counterAttack: 10 },
+    "bard": { name: "Bard", image: '<img src="assets/images/hero_bard.png">', health: 150, attack: 15, counterAttack: 10 },
+    "warrior": { name: "Warrior", image: '<img src="assets/images/hero_warrior.png">', health: 100, attack: 25, counterAttack: 10 }
 }
 
 //global variables for refernerenenerece
@@ -21,14 +21,16 @@ $(document).ready(function () {
 
     //character select
 $('.character').click(function() {
-    if (playerSet == false) {
-        for (var i =0; i < characters.length; i++) {
-            if (characters[i] == this) {
-                $('#player').append(characters[i]);
-                playerSet = true;
-            }
-        }
-    }
+    console.log($(this).attr('id') );
+debugger;
+    //if (playerSet == false) {
+    //    for (var i =0; i < characters.length; i++) {
+    //        if (characters[i] == this) {
+    //            $('.playerarea').append(characters[i]);
+    //            playerSet = true;
+    //        }
+    //    }
+    //}
 })
     //enemy select
 
