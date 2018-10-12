aab40c7b8d8d99ce3ef79a58ae5c79d654278f12
Justin Honegger <justinhonegger@doublefine.com>
2018-10-12 13:36:10 -0700

    SO MUCh LEARNING

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,38 @@
 
 
 var characters = {
-    "wizard": { name: "<h2>Wizard</h2>", playerImage: '<img src="assets/images/hero_wizard.png">', enemyImage: '<img src="assets/images/villain_wizard.png">', health: 80, attack: 30, counterAttack: 30 },
-    "paladin": { name: "<h2>Paladin</h2>", playerImage: '<img src="assets/images/hero_paladin.png">', enemyImage: '<img src="assets/images/villain_paladin.png">', health: 200, attack: 10, counterAttack: 10 },
-    "bard": { name: "<h2>Bard</h2>", playerImage: '<img src="assets/images/hero_bard.png">', enemyImage: '<img src="assets/images/villain_bard.png">', health: 150, attack: 15, counterAttack: 15 },
-    "warrior": { name: "<h2>Warrior</h2>", playerImage: '<img src="assets/images/hero_warrior.png">', enemyImage: '<img src="assets/images/villain_warrior.png">', health: 100, attack: 25, counterAttack: 25 }
+    "wizard": {
+        name: "Wizard",
+        playerImage: '<img src="assets/images/hero_wizard.png">',
+        enemyImage: '<img src="assets/images/villain_wizard.png">',
+        health: 80,
+        attack: 30,
+        counterAttack: 30
+    },
+    "paladin": {
+        name: "<h2>Paladin</h2>",
+        playerImage: '<img src="assets/images/hero_paladin.png">',
+        enemyImage: '<img src="assets/images/villain_paladin.png">',
+        health: 200,
+        attack: 10,
+        counterAttack: 10
+    },
+    "bard": {
+        name: "<h2>Bard</h2>",
+        playerImage: '<img src="assets/images/hero_bard.png">',
+        enemyImage: '<img src="assets/images/villain_bard.png">',
+        health: 150,
+        attack: 15,
+        counterAttack: 15
+    },
+    "warrior": {
+        name: "<h2>Warrior</h2>",
+        playerImage: '<img src="assets/images/hero_warrior.png">',
+        enemyImage: '<img src="assets/images/villain_warrior.png">',
+        health: 100,
+        attack: 25,
+        counterAttack: 25
+    }
 }
 
 //global variables for refernerenenerece
@@ -12,7 +40,6 @@ var attack = 0;
 var enemySet = false;
 var playerSet = false;
 var gameStart = false;
-
 
 $(document).ready(function () {
 
@@ -29,26 +56,27 @@ $(document).ready(function () {
     setUp();
     //character select
     $('.character').click(function () {
-       if (playerSet == false){
-        $(".playerarea").show();
-        $(".playerarea").append($(this).attr('id'));
-        $(this).hide();
-        playerSet = true;
-       } else if (playerSet == true && enemySet == false) {
-        $(".enemyarea").show();
-        $(".attackbtn").show();
-        $(".enemyarea").append($(this).attr('id'));
-        $(this).hide();
-        enemySet = true;
-       }
-        //if (playerSet == false) {
-        //    for (var i = 0; i < characters.length; i++) {
-        //        if (characters[i] == $(this).attr.id) {
-        //            $('.playerarea').innerhtml(characters[i]);
-        //            
-        //        }
-        //    }
-        //}
+        if (playerSet === false && enemySet === false) {
+            $(this).removeClass("character");
+            $(".innerplayer").replaceWith($(this));
+            $(".playerarea").show();
+            playerSet = true;
+        }
+        else if (playerSet === true && enemySet === false) {
+            $(".innerenemy").replaceWith($(this));
+            $(".attackbtn").show();
+            $(".enemyarea").show();
+            enemySet = true;
+        }
+    })
+
+    $('.attackbtn').click(function (){
+        player = $(".playerarea").children("div").eq(0).attr('id');
+        enemy = $(".enemyarea").children("div").eq(0).attr('id');
+        console.log(player +' VS '+enemy);
+        playerObj = characters[player];
+        enemyObj = characters[enemy];
+        console.log(playerObj.name);
     })
     //enemy select
 
